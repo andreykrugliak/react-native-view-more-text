5e556bde19273998769800ef8f847e1172dd1350
Krugliak <andrey.krugliak@litslink.com>
2019-06-12 16:40:33 +0300

    type fix

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,18 @@
 import * as React from 'react';
 import { ViewProperties, StyleProp, TextStyle } from 'react-native';
-  
-declare module 'react-native-view-more-text' {
 
+declare module 'react-native-view-more-text' {
   export interface ViewMoreTextProps extends ViewProperties {
     numberOfLines: number;
     renderViewMore?: (handlePress: () => void) => JSX.Element;
-    renderViewLess?: (handlePress: () => void) => JSX.Element;
-    afterCollapse?: () => void
-    afterExpand?: () => void
+    renderViewMore?: (handlePress: () => void) => JSX.Element;
+    defaultFooter?: () => JSX.Element;
+    afterCollapse?: () => void;
+    afterExpand?: () => void;
     textStyle?: StyleProp<TextStyle>;
   }
-  
-  export default class ViewMoreText extends React.Component<ViewMoreTextProps> {}
+
+  export default class ViewMoreText extends React.Component<
+    ViewMoreTextProps
+  > {}
 }
